760e345980e5b8126591ceac74527383724e3117
Alvaro Sacari <alvaro.sacari@gmail.com>
2020-03-06 09:41:35 -0500

    updated Contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link, Prompt } from 'react-router-dom'
 import './Contact.css';
 
 function Contact () {
   return (
     <div className="Contact">
       <h1>Contact</h1>
+      <Link
+        className="App-link"
+        to="/"
+      >
+        Home page
+      </Link>
+
+      {
+        <Prompt
+          when={true}
+          message="Are you sure you want to go to Home page?"
+        />
+      }
     </div>
   );
 }
