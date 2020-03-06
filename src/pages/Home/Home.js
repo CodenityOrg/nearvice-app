ba599421131f421be2f040fc8f9872c12e660dff
Alvaro Sacari <alvaro.sacari@gmail.com>
2020-03-06 09:41:10 -0500

    Home page has been updated

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import logo from '../../logo.svg';
 import './Home.css';
+import { useTranslation } from 'react-i18next';
 
 function Home () {
+  const { t } = useTranslation();
+  const tMessage = t('example.nestedMessage.identifier');
+
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {tMessage}
+        </p>
+        <Link
+          className="App-link"
+          to="/contact"
+        >
+          Contact page
+        </Link>
+      </header>
     </div>
   );
 }
