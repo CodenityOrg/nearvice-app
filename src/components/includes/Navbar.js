2a2b4b4d9c7a5b2e59d9eafa17a26b8e3e125655
Alvaro Sacari <alvaro.sacari@gmail.com>
2020-03-10 18:11:37 -0500

    class component has been converted to function component

--- src/components/includes/Navbar.js
+++ src/components/includes/Navbar.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.title}
-      </div>
-    )
-  }
+function Navbar (props) {
+  return (
+    <div>
+      {props.title}
+    </div>
+  )
 }
 
 Navbar.propTypes = {
