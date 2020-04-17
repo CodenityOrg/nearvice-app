0f4975f925300ccc5aa32158d85fff47ed0a2fed
Florencia Peretti <florenperetti@gmail.com>
2020-04-17 15:43:29 -0300

    remove test hook

--- src/App.js
+++ src/App.js
@@ -14,25 +14,9 @@ import LoginModal from './components/LoginModal';
 import SideDrawer from './components/includes/SideDrawer/index.js';
 
 import useBoolToggler from './hooks/boolToggler';
-function useTraceUpdate (props) {
-  const prev = React.useRef(props);
-  React.useEffect(() => {
-    const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
-      if (prev.current[k] !== v) {
-        ps[k] = [prev.current[k], v];
-      }
-      return ps;
-    }, {});
-    if (Object.keys(changedProps).length > 0) {
-      console.log('Changed props:', changedProps);
-    }
-    prev.current = props;
-  });
-}
 
 function App (props) {
 
-  useTraceUpdate(props);
   const [isLoginVisible, setLoginVisibleTrue, setLoginVisibleFalse] = useBoolToggler();
   const [isRegisterVisible, setRegisterVisibleTrue, setRegisterVisibleFalse] = useBoolToggler();
 
