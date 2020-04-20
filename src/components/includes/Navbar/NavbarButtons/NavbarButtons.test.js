171b77f95710d4082c44d51d720f32c5832d6d1a
Florencia Peretti <florenperetti@gmail.com>
2020-04-20 16:11:22 -0300

    improve code

--- src/components/includes/Navbar/NavbarButtons/NavbarButtons.test.js
+++ src/components/includes/Navbar/NavbarButtons/NavbarButtons.test.js
@@ -7,17 +7,15 @@ test('renders sign in and sign up buttons if user is not authenticated', () => {
   const { getByText } = render(<MemoryRouter>
     <NavbarButtons onSignInClick={() => { }} onSignUpClick={() => { }} isAuth={false} t={key => key} />
   </MemoryRouter>);
-  const signInButton = getByText('Sign In');
-  const signUpButton = getByText('Sign Up');
-  expect(signInButton).toBeInTheDocument();
-  expect(signUpButton).toBeInTheDocument();
+  expect(getByText(/sign in/i)).toBeInTheDocument();
+  expect(getByText(/sign up/i)).toBeInTheDocument();
 });
 
 test('doesn\'t render sign in and sign up buttons if user is authenticated and renders avatar', () => {
   const { queryByText, getByTestId } = render(<MemoryRouter>
     <NavbarButtons onSignInClick={() => { }} onSignUpClick={() => { }} user={{ profilePicture: '' }} isAuth={true} t={key => key} />
   </MemoryRouter>);
-  expect(queryByText('Sign In')).toBeNull()
-  expect(queryByText('Sign Up')).toBeNull()
+  expect(queryByText(/sign in/i)).toBeNull()
+  expect(queryByText(/sign up/i)).toBeNull()
   expect(getByTestId('user-avatar')).toBeInTheDocument();
 });
