8a255994c02ebd03baea1c1aee4036a18c15a3f1
Angel <aleangrodriguez123@gmail.com>
2020-04-30 22:40:59 -0500

    update image container by branch name

--- conf/deploy.sh
+++ conf/deploy.sh
@@ -2,7 +2,7 @@
 set -e # Stop script from running if there are any errors
 
 IMAGE="codenity/nearvice-app"                             # Docker image
-GIT_VERSION=$(git describe --always --abbrev --tags --long) # Git hash and tags
+GIT_VERSION=$(git rev-parse --abbrev-ref HEAD) # Git hash and tags
 
 # Build and tag image
 docker build -t ${IMAGE}:${GIT_VERSION} .
