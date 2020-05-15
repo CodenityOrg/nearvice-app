bce3d8a45f44de51c278885623fd4b1c857cefdd
Angel <aleangrodriguez123@gmail.com>
2020-05-15 17:26:19 -0500

    add encrypted key for deployment

--- conf/deploy.sh
+++ conf/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e # Stop script from running if there are any errors
 
-IMAGE="codenity/nearvice-app"                             # Docker image
-GIT_VERSION=$(git rev-parse --abbrev-ref HEAD) # Git hash and tags
-
 docker push ${IMAGE}:${GIT_VERSION}
+
+ssh root@138.197.195.221 \ 'source ~/.zshrc; cd scripts; bash nearvice-api-deploy.sh'
