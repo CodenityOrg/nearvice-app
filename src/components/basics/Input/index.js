2071e89580b163a39db47edc9a3ede5daefd2d10
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-19 14:20:44 -0500

    lint Input component

--- src/components/basics/Input/index.js
+++ src/components/basics/Input/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Styled from './styles';
 
-export default props => {
+const Input = props => {
     return <Styled.Input 
         {
             ...props
         }
     />;
 };
+
+export default Input;
